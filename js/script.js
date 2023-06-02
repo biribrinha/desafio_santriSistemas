2efa8aba74add692030d544281a74d0d4dc9f54f
juliabergman <juliabergman523@gmail.com>
2023-06-02 01:55:18 -0300

    feat: animação js ok

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let menu = document.getElementById("menu_links");
+let menu_barras = document.getElementById("menu_barras");
+let minus = document.getElementById("minus");
+let recursos = document.getElementById("recursos");
+
+function mostrarMenu() {
+    /* verificando a situacao do display menu */
+    if (window.getComputedStyle(menu).display == "none") {
+
+        menu.style.display = "initial"
+        menu_barras.setAttribute("aria-label", "fechar menu")
+        menu_barras.setAttribute("aria-expanded", "true")
+
+
+
+    } else {
+        menu.style.display = "none"
+        menu_barras.setAttribute("aria-label", "abrir menu")
+        menu_barras.setAttribute("aria-expanded", "false")
+    }
+}
+
+function mostrarMinus() {
+    if (window.getComputedStyle(recursos).display == "none") {
+
+        recursos.style.display = "initial"
+        minus.setAttribute("aria-label", "Esconder Recursos")
+        minus.setAttribute("aria-expanded", "true")
+    } else{
+        recursos.style.display = "none"
+        minus.setAttribute("aria-label", "Mostrar Recursos")
+        minus.setAttribute("aria-expanded", "false")
+    }
+}
