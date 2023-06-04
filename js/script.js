546911adbbb18d3657a4a3e103affc429e70e1df
juliabergman <juliabergman523@gmail.com>
2023-06-04 16:25:41 -0300

    fix: ajustes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
 let menu = document.getElementById("menu_links");
+
 let menu_barras = document.getElementById("menu_barras");
 let minus = document.getElementById("minus");
 let recursos = document.getElementById("recursos");
 
+
+let minusTop = Array.from(document.getElementsByClassName("minusTop"));
+
+
 let images = Array.from(document.getElementsByClassName("imgCarousel"));
 let mainPhoto = document.getElementById("mainPhoto");
+
+
+
+
+
+
+function mudarImagem(event) {
+    let image = event.target;
+    mainPhoto.src = image.src
+};
+
+images.forEach(function (image) {
+    image.addEventListener("click", mudarImagem);
+});
+
+
+function trocarMinus() {
+    if (window.getComputedStyle(minus).display == "none") {
+        minus.style.display = "initial"
+
+    }
+}
 
 function mostrarMenu() {
     /* verificando a situacao do display menu */
     if (window.getComputedStyle(menu).display == "none") {
 
-        menu.style.display = "initial"
+        menu.style.display = "flex"
         menu_barras.setAttribute("aria-label", "fechar menu")
         menu_barras.setAttribute("aria-expanded", "true")
 
@@ -29,6 +56,8 @@ function mostrarMinus() {
         recursos.style.display = "initial"
         minus.setAttribute("aria-label", "Esconder Recursos")
         minus.setAttribute("aria-expanded", "true")
+
+
     } else {
         recursos.style.display = "none"
         minus.setAttribute("aria-label", "Mostrar Recursos")
@@ -37,12 +66,10 @@ function mostrarMinus() {
 };
 
 
-
-function mudarImagem(event) {
-    let image = event.target;
-    mainPhoto.src = image.src
-};
-
-images.forEach(function(image) {
-    image.addEventListener("click", mudarImagem);
+//delay 
+ScrollReveal().reveal('.card_depoimentos', {
+    interval: 300
 });
+
+
+
